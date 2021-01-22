2aee8d7f551cad718c1ae82d92a6491d07429ff6
Yuki Sato <yuki.sato@lightcurve.io>
2021-01-22 22:59:31 +0100

    Add initialize table query

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT
 const { postUrl } = require('./controller')
+const db = require('./db');
 
 app.use(bodyParser.json())
 app.use(
@@ -17,7 +18,9 @@ app.get('/', (request, response) => {
 })
 
 app.post('/shorturl', postUrl)
+const initalizeTable_query = `CREATE TABLE IF NOT EXISTS urls (req_url TEXT NOT NULL,short_id CHAR (8) UNIQUE NOT NULL)`
 
 app.listen(port, async () => {
   console.log(`App running on port ${port}.`)
+  db.query(initalizeTable_query)
 });
