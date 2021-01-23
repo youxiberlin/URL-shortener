5f3894932c0bcc246e84e435c8f18b69799eefb6
Yuki Sato <yuki.sato@lightcurve.io>
2021-01-23 22:18:54 +0100

    Add table ips and insert request ips

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(
     extended: true,
   })
 );
+app.set('trust proxy', true)
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
@@ -19,9 +20,11 @@ app.get('/', (request, response) => {
 
 app.post('/shorturl', postUrl);
 app.get('/:id', getOriginalUrl);
-const initalizeTable_query = `CREATE TABLE IF NOT EXISTS urls (req_url TEXT NOT NULL,short_id CHAR (8) UNIQUE NOT NULL)`
+const initalizeTable_urls = `CREATE TABLE IF NOT EXISTS urls (req_url TEXT NOT NULL,short_id CHAR (8) UNIQUE NOT NULL)`
+const initalizeTable_ips = `CREATE TABLE IF NOT EXISTS ips (req_ip TEXT NOT NULL,short_id CHAR (8) NOT NULL)`
 
 app.listen(port, async () => {
   console.log(`App running on port ${port}.`)
-  db.query(initalizeTable_query)
+  db.query(initalizeTable_urls)
+  db.query(initalizeTable_ips)
 });
--- controllers.js
+++ controllers.js
@@ -6,11 +6,10 @@ const postUrl = async (req, res, next) => {
   const id = nanoid(8);
   const shortUrl = `tier.app/${id}`;
   
-  await db.query('INSERT INTO urls (req_url, short_id) VALUES ($1, $2)', [url, id], (error, results) => {
-    if (error) {
-      throw error
-    }
-  });
+  await db.query('INSERT INTO urls (req_url, short_id) VALUES ($1, $2)', [url, id])
+    .then(result => console.log('result', result))
+    .catch(e => console.error(e.stack))
+
 
   res.status(200).json({
       status: "Success!",
@@ -19,6 +18,10 @@ const postUrl = async (req, res, next) => {
 };
 
 const getOriginalUrl = async (req, res, next) => {
+  db.query('INSERT INTO ips (req_ip, short_id) VALUES ($1, $2)', [req.ip, req.params.id])
+    .then(result => console.log(result))
+    .catch(e => console.error(e.stack))
+
   db.query(`SELECT req_url FROM urls WHERE short_id='${req.params.id}'`)
     .then(async result => {
       const dbResult = await result.rows[0];
