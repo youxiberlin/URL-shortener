c6d52dee1719bfb5db116d56944422ec70f8a69b
Yuki Sato <yuki.sato@lightcurve.io>
2021-01-24 14:00:27 +0100

    Use redis in controller

--- controllers.js
+++ controllers.js
@@ -1,13 +1,18 @@
 const db = require('./services/db');
 const { nanoid } = require('nanoid');
+const redis = require("redis");
+const client = redis.createClient();
 
 const postUrl = async (req, res, next) => {
   const { url } = req.body;
   const id = nanoid(8);
   const shortUrl = `tier.app/${id}`;
+  client.set(id, url, (err, res) => {
+    if (err) console.error('Redis error', err)
+  });
   
   await db.query('INSERT INTO urls (req_url, short_id) VALUES ($1, $2)', [url, id])
-    .then(result => console.log('result', result))
+    .then(result => console.log(`Inserted the URL with id ${id} to DB`))
     .catch(e => {
       console.error(e.stack);
       res.status(500).json({
@@ -23,16 +28,10 @@ const postUrl = async (req, res, next) => {
 };
 
 const getOriginalUrl = async (req, res, next) => {
-  db.query('INSERT INTO ips (req_ip, short_id) VALUES ($1, $2)', [req.ip, req.params.id])
-    .then(result => console.log(result))
-    .catch(e => {
-      console.error(e.stack);
-      res.status(500).json({
-        status: "Error",
-        result: "Server error"
-      });
-    })
-
+  client.get(req.params.id, (err, res) => {
+    console.log(`Redis returns: ${res}`)
+    if (err) console.error('Redis error', err)
+  });
   db.query(`SELECT req_url FROM urls WHERE short_id='${req.params.id}'`)
     .then(async result => {
       const dbResult = await result.rows[0];
@@ -47,6 +46,16 @@ const getOriginalUrl = async (req, res, next) => {
       }
     })
     .catch(e => console.error(e.stack));
+  db.query('INSERT INTO ips (req_ip, short_id) VALUES ($1, $2)', [req.ip, req.params.id])
+    .then(result => console.log(result))
+    .catch(e => {
+      console.error(e.stack);
+      res.status(500).json({
+        status: "Error",
+        result: "Server error"
+      });
+    })
+
 };
 
 const getStats = async (req, res, next) => {
