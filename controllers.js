d695eb79f1d74767d1e4575c5f8b1a65f5cacbc9
Yuki Sato <yuki.sato@lightcurve.io>
2021-01-24 15:57:23 +0100

    Improve logic of getOriginalUrl controller

--- controllers.js
+++ controllers.js
@@ -2,7 +2,7 @@ const { nanoid } = require('nanoid');
 const db = require('./services/db');
 const { getAsync, setAsync } = require('./services/redis');
 
-const postUrl = async (req, res, next) => {
+const postUrl = async (req, res) => {
   const { url } = req.body;
   const id = nanoid(8);
   const shortUrl = `tier.app/${id}`;
@@ -26,7 +26,7 @@ const postUrl = async (req, res, next) => {
   });
 };
 
-const getOriginalUrl = async (req, res, next) => {
+const getOriginalUrl = async (req, res) => {
   const { id } = req.params;
 
   const redisRes = await getAsync(id)
@@ -36,10 +36,15 @@ const getOriginalUrl = async (req, res, next) => {
         status: "Error",
         result: "Server error"
       });
-    })
+    });
 
   if (redisRes) {
-    res.redirect(`http://${redisRes}`)
+    res.redirect(`http://${redisRes}`);
+    db.query('INSERT INTO ips (req_ip, short_id) VALUES ($1, $2)', [req.ip, id])
+      .then(() => console.log(`Inserted ip ${req.ip} to ${id} in DB`))
+      .catch(e => {
+        console.error(e.stack);
+      });
   } else {
     const dbRes = await db.query(`SELECT req_url FROM urls WHERE short_id='${id}'`)
       .catch(e => {
@@ -53,7 +58,12 @@ const getOriginalUrl = async (req, res, next) => {
     const result = dbRes.rows[0];
     if (result) {
       const { req_url } = result;
-      res.redirect(`http://${req_url}`)
+      res.redirect(`http://${req_url}`);
+      db.query('INSERT INTO ips (req_ip, short_id) VALUES ($1, $2)', [req.ip, id])
+        .then(result => console.log(`Inserted ip ${req.ip} to ${id} in DB`))
+        .catch(e => {
+          console.error(e.stack);
+        });
     } else {
       res.status(400).json({
           status: "Not found",
@@ -61,15 +71,9 @@ const getOriginalUrl = async (req, res, next) => {
         })
     }
   }
-  
-  db.query('INSERT INTO ips (req_ip, short_id) VALUES ($1, $2)', [req.ip, id])
-    .then(result => console.log(`Inserted ip ${req.ip} to ${id} in DB`))
-    .catch(e => {
-      console.error(e.stack);
-    })
 };
 
-const getStats = async (req, res, next) => {
+const getStats = async (req, res) => {
   const results = await db.query('SELECT short_id, COUNT (short_id) FROM ips GROUP BY short_id')
     .catch(e => {
       console.error(e.stack)
@@ -89,7 +93,7 @@ const getStats = async (req, res, next) => {
     status: "Success",
     data: dataObj
   });
-}; 
+};
 
 module.exports = {
   postUrl,
