3b73e6bb799af1789075496a437676ea8ac9702f
Yuki Sato <yuki.sato@lightcurve.io>
2021-01-30 13:31:27 +0100

    Add db connection to server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { app } = require('./app.js')
 const { port } = require('./config');
+const dbConfig = require('./config').postgres;
 const db = require('./services/db');
 
 const initalizeTable_urls = `CREATE TABLE IF NOT EXISTS urls (req_url TEXT NOT NULL,short_id CHAR (8) UNIQUE NOT NULL)`
@@ -7,6 +8,7 @@ const initalizeTable_ips = `CREATE TABLE IF NOT EXISTS ips (req_ip TEXT NOT NULL
 
 app.listen(port, async () => {
   console.log(`App running on port ${port}`);
+  await db.connect(dbConfig);
   db.query(initalizeTable_urls);
   db.query(initalizeTable_ips);
 });
--- services/db.js
+++ services/db.js
@@ -1,18 +1,20 @@
 const { Pool } = require('pg');
-const { user, host, database, password, port } = require('./../config').postgres;
+const dbConfig = require('./../config').postgres;
 
 const connPool = {};
 
-const connect = (endpoint) => {
-	if (!connPool[endpoint]) {
-		connPool[endpoint] = new Pool({ user, host, database, password, port });
-		console.log(`Postgres: Connected on port ${endpoint}`);
+const connect = (config) => {
+	const { user, host, database, password, port } = config;
+	if (!connPool[port]) {
+		connPool[port] = new Pool({ user, host, database, password, port });
+		console.log(`Postgres: Connected on port ${port}`);
 	}
-	return connPool[endpoint];
+	return connPool[port];
 };
 
-const db = connect(port);
+const db = connect(dbConfig);
 
 module.exports = {
+	connect: (config) => connect(config),
   query: (text, params) => db.query(text, params),
 };
